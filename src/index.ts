9225e7555c7fe087dc96461b61f33bc13655b4bd
Luciano Sabino <lb255043@gmail.com>
2024-11-08 07:35:06 -0300

    Atualização de porta para o render

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { app } from "./server/server";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
 
 app.listen(port, () => console.log(`Servidor rodando! na porta ${port}`));
